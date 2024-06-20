9616eb05847d6ec3bb5916a486d1f2c195e54e2b
SUNIL SURENDRAN <133184647+SunilSurendran1906@users.noreply.github.com>
2024-06-20 12:58:31 +0530

    update env file in db.js

--- config/db.js
+++ config/db.js
@@ -1,11 +1,16 @@
+// db.js or wherever you handle the database connection
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+// Load environment variables from .env file
+dotenv.config();
 
 export const connectDB = async () => {
   await mongoose
-    .connect(
-      "mongodb+srv://rvsunil0603:rvs19092000@food-order-app.07jxxxq.mongodb.net/food-del"
-    )
-    .then(() => console.log("DB Connected"));
+    .connect(process.env.MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => console.log("DB Connected"))
+    .catch((err) => console.error("DB Connection Error: ", err));
 };
-
-
